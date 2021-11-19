0605f43d9dc132bb64e57a8ece563cc224365a57
Dave Fernandes <dave.analista@gmail.com>
2021-11-19 19:29:04 -0300

    adicionado o exception handler de acesso negado: org.springframework.security.access.AccessDeniedException

--- src/main/java/com/dave/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/dave/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -4,10 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 
 import com.dave.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.dave.helpdesk.services.exceptions.ObjectnotFoundException;
@@ -47,6 +49,16 @@ public class ResourceExceptionHandler {
 		}
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+	}
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<StandardError> accessDeniedException(AccessDeniedException ex,
+			HttpServletRequest request) {
+
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(),
+				"Acesso Negado!", ex.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
 	}
 }
 
