e3b19c4a29a82b65393e6cf7b2134cf9407b54db
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-08-18 17:33:55 -0300

    feature: order by chamado.id desc em listChamados

--- src/main/java/com/dave/helpdesk/repositories/ChamadoRepository.java
+++ src/main/java/com/dave/helpdesk/repositories/ChamadoRepository.java
@@ -10,7 +10,7 @@ import com.dave.helpdesk.domain.Chamado;
 public interface ChamadoRepository extends JpaRepository<Chamado, Integer> {
 	
 	
-	@Query(value = "SELECT * FROM CHAMADO C WHERE ( C.TITULO ILIKE CONCAT('%', :titulo,'%') OR C.OBSERVACOES ILIKE CONCAT('%', :titulo,'%') ) AND C.STATUS IN (:status)", nativeQuery = true)
+	@Query(value = "SELECT * FROM CHAMADO C WHERE ( C.TITULO ILIKE CONCAT('%', :titulo,'%') OR C.OBSERVACOES ILIKE CONCAT('%', :titulo,'%') ) AND C.STATUS IN (:status) ORDER BY C.ID DESC", nativeQuery = true)
 	List<Chamado> findByFilter(String titulo, int[] status);
 
 }
