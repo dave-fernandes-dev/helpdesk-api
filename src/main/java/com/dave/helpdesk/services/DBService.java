db7dcbcd472f201e826504e0e751f2881ff21cf1
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2023-05-24 20:40:22 -0300

    feature: mais chamados para teste

--- src/main/java/com/dave/helpdesk/services/DBService.java
+++ src/main/java/com/dave/helpdesk/services/DBService.java
@@ -50,14 +50,21 @@ public class DBService {
 		Cliente cli9 = new Cliente(null, "Cliente 9", "48674650031", "cli9@mail.com", encoder.encode("123"));
 		Cliente cli10 = new Cliente(null, "Cliente 10", "05967418040", "cli10@mail.com", encoder.encode("123"));		
 		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 1", "Teste chamado 1", tec1, cli1);
-		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 2", "Teste chamado 2", tec1, cli2);
-		Chamado c3 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 3", "Teste chamado 3", tec2, cli3, LocalDate.now());
-		Chamado c4 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 4", "Teste chamado 4", tec3, cli3);
-		Chamado c5 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 5", "Teste chamado 5", tec2, cli1);
-		Chamado c6 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 7", "Teste chamado 6", tec1, cli5, LocalDate.now());
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Consertar Notebook", "notebook não liga", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Consertar Monitor", "tela azul", tec1, cli2);
+		Chamado c3 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Monitor com Defeito", "Teste chamado 3", tec2, cli3, LocalDate.now());
+		Chamado c4 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "CPU não liga", "não sei o motivo", tec3, cli3);
+		Chamado c5 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "mal contato pen drive", "Teste chamado 5", tec2, cli1);
+		Chamado c6 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "celular trincado", "Teste chamado 6", tec1, cli5, LocalDate.now());
+		Chamado c7= new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 1", "Teste chamado 1", tec1, cli1);
+		Chamado c8= new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 2", "Teste chamado 2", tec1, cli2);
+		Chamado c9= new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 3", "Teste chamado 3", tec2, cli3, LocalDate.now());
+		Chamado c10= new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 4", "Teste chamado 4", tec3, cli3);
+		Chamado c11= new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 5", "Teste chamado 5", tec2, cli1);
+		Chamado c12= new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 7", "Teste chamado 6", tec1, cli5, LocalDate.now());
+
 
 		pessoaRepository.saveAll(Arrays.asList(tec0, tec1, tec2, tec3, tec4, tec5, cli1, cli2, cli3, cli4, cli5, cli6, cli7, cli8, cli9, cli10));
-		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6));
+		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6,c7, c8, c9, c10, c11, c12));
 	}
 }
