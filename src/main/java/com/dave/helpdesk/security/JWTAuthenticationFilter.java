24e82cb82339f155b5627598e69da41114c95dd6
Dave Fernandes <dave.fernandes.dev@gmail.com>
2023-06-27 20:53:38 -0300

    tirando senha

--- src/main/java/com/dave/helpdesk/security/JWTAuthenticationFilter.java
+++ src/main/java/com/dave/helpdesk/security/JWTAuthenticationFilter.java
@@ -71,8 +71,8 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		return "{"
 				+ "\"timestamp\": " + date + ", " 
 				+ "\"status\": 401, "
-				+ "\"error\": \"Não autorizado\", "
-				+ "\"message\": \"Email ou senha inválidos\", "
+				+ "\"error\": \"Nao autorizado\", "
+				+ "\"message\": \"Email ou senha invalidos\", "
 				+ "\"path\": \"/login\"}";
 	}
 	
